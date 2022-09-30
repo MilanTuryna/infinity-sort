54587341a594f865378e14ff61b02ee728ec2025
MilanTuryna <miloslavturyna@seznam.cz>
2022-09-30 21:08:44 +0200

    Redesigned old version to OOP (created two classes SortObject, SortManager) and added functions for easier use by client, in future we will add some methods for rows managing (like getBy...) in sortManager [(infinity-sort.js)]

--- infinity-sort.js
+++ infinity-sort.js
@@ -1,6 +1,9 @@
 // DB recursion test, it's used for logical principle, we will use for article categories in the future.
 // Example is created by car database.
-class SortObject {
+"use strict";
+
+export class SortObject {
+    static properties = Object.getOwnPropertyNames(new SortObject());
     constructor(name, level, parent_id, id) {
         this.name = name;
         this.level = level;
@@ -11,47 +14,49 @@ class SortObject {
     setChildren(children) {
         this.children = children;
     }
-}
 
-let objectResult = [];
-function recursion(database) {
-    let firstLevels = database.filter(obj => obj.level === 0);
-    firstLevels.forEach(firstLevel => {
-        let children = database.filter(obj => obj.parent_id === firstLevel.id);
-        let newEntry = new SortObject(firstLevel.name, firstLevel.level, firstLevel.parent_id, firstLevel.id);
-        children.forEach(cc => {
-            function rec(level, otherLevels) {
-                otherLevels.forEach(obj => {
-                    children[children.indexOf(cc)].children.push(obj)
-                    let nextLevel = level+1;
-                    rec(nextLevel, database.filter(obj => obj.level === nextLevel && obj.parent_id === obj.id))
-                })
-            }
-            rec(2, database.filter(obj => obj.level === 2 && obj.parent_id === cc.id));
+    static createFrom(obj) {
+        if(!SortObject.properties.some((el) => Object.getOwnPropertyNames(obj).includes(el))) {
+            throw TypeError("Object must have these properties: " + SortObject.properties);
+        }
+        let sortObject = new SortObject(obj.name, obj.level, obj.parent_id, obj.id);
+        Object.entries((key, value) => sortObject[key] = value); // instead {...sortObject, obj} because we need save setChildren() method with object
+        return sortObject
+    }
+}
+export class SortManager {
+    _createValidObjects() {
+        this.rows = this.rows.map(SortObject.createFrom);
+    }
+    _sort() {
+        let levels1 = this.rows.filter(obj => obj.level === 0);
+        levels1.forEach(firstLevel => {
+            let children = this.rows.filter(obj => obj.parent_id === firstLevel.id);
+            let newEntry = SortObject.createFrom(firstLevel);
+            children.forEach((cc) => {
+                let infinityLevel = (level, otherLevels) => {
+                    otherLevels.forEach((obj) => {
+                        children[children.indexOf(cc)].children.push(obj)
+                        let nextLevel = level+1;
+                        infinityLevel(nextLevel, this.rows.filter(obj => obj.level === nextLevel && obj.parent_id === obj.id))
+                    })
+                }
+                infinityLevel(2, this.rows.filter(obj => obj.level === 2 && obj.parent_id === cc.id))
+            })
+            newEntry.setChildren(children);
+            this.sortedRows.push(newEntry);
         })
-        newEntry.setChildren(children);
-        objectResult.push(newEntry);
-    })
+    }
+
+    constructor(...rows) {
+        this.rows = rows;
+        this.sortedRows = [];
+
+        this._createValidObjects()
+        this._sort();
+    }
+
+    getHierarchy() {
+        return this.sortedRows;
+    }
 }
-
-// test
-let database = [
-    new SortObject("Auta", 0, 0, 1),
-    new SortObject("Závodní", 1, 1, 2),
-    new SortObject("Nákladní", 1, 1, 3),
-    new SortObject("Motorky", 0, 0, 4),
-    new SortObject("Enduro", 1, 4, 5),
-    new SortObject("Babety", 1, 4, 6),
-    new SortObject("Letadla", 0, 0, 7),
-    new SortObject("Veřejná", 1, 7, 8),
-    new SortObject("Airbus", 2, 8, 9),
-    new SortObject("Boieng", 2, 8, 10),
-    new SortObject("707", 3, 10, 11),
-    new SortObject("SmartWings", 2, 8, 12),
-    new SortObject("Lodě", 0, 0, 13)
-]
-
-recursion(database);
-console.log(JSON.stringify(objectResult, null, "\t"));
-
-
