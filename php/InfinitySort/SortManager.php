0e27e1c1f4da9209d22fe0107bf9713e2e79781d
MilanTuryna <miloslavturyna@seznam.cz>
2023-01-23 16:42:12 +0100

    fixed sortmanager (fixed results coming in associative array and fixed support for really infinity nest)

--- php/InfinitySort/SortManager.php
+++ php/InfinitySort/SortManager.php
@@ -16,21 +16,26 @@ class SortManager
 
     /**
      * @param array $rows
-     * @param bool $useAssociativeID
      */
-    public function __construct(array $rows, bool $useAssociativeID = false) {
+    public function __construct(array $rows) {
         $this->rows = $rows;
 
-        $this->sort($useAssociativeID);
+        $this->sort();
+    }
+
+    /**
+     * @param ...$params
+     */
+    private function debug(... $params): void {
+        echo json_encode([...$params]) . "\n";
     }
 
     // TODO: Change variable names, because it's so confusing
 
     /**
-     * @param bool $useAssociativeID
      * @return void
      */
-    private function sort(bool $useAssociativeID = false): void {
+    private function sort(): void {
         $levels1 = array_filter($this->rows, fn(sortObject $obj) => $obj->parent_id == 0);
         foreach ($levels1 as $firstLevel) {
             $firstLevel->level = 0;
@@ -38,19 +43,24 @@ class SortManager
             $newEntry = sortObject::init()->createFrom($firstLevel);
             foreach ($childrenOf1 as $key => $child1) {
                 $childrenOf1[$key]->level = 1; $child1->level = 1;
-                $calculateInfinityLevel = function ($level, $otherLevels) use (&$calculateInfinityLevel, &$childrenOf1, $child1, $key) {
-                    foreach ($otherLevels as $obj) {
+                $calculateInfinityLevel = function ($level, $otherLevels, &$superior, $k) use (&$calculateInfinityLevel) {
+                    foreach ($otherLevels as $objKey => $obj) {
                         $obj->level = $level;
-                        $childrenOf1[$key]->children[] = $obj;
+                        $this->debug($superior, $obj, $k);
+                        $superior[$k]->children[] = $obj;
                         $nextLevel = $level + 1;
-                        $filter = fn(sortObject $obj) => $obj->level === $nextLevel && $obj->parent_id === $obj->id;
-                        $calculateInfinityLevel($nextLevel, array_filter($this->rows, $filter));
+                        $filter = fn(sortObject $e) => $e->parent_id === $obj->id;
+                        // used instead => foreach because php it's weird and when i use it, it will change original value into ass. array
+                        $childrenKey = array_search($obj, $superior[$k]->children);
+                        $calculateInfinityLevel($nextLevel, array_filter($this->rows, $filter),
+                            $superior[$k]->children,
+                            $childrenKey);
                     }
                 };
-                $filter = fn(sortObject $obj) => $obj->level === 2 && $obj->parent_id === $child1->id;
-                $calculateInfinityLevel(2, array_filter($this->rows, $filter));
+                $filter = fn(sortObject $obj) => $obj->parent_id === $child1->id;
+                $calculateInfinityLevel(2, array_filter($this->rows, $filter), $childrenOf1, $key);
             }
-            $newEntry->children = $useAssociativeID ? $childrenOf1 : array_values($childrenOf1);
+            $newEntry->children = array_values($childrenOf1);
             $this->sortedRows[] = $newEntry;
         }
     }
