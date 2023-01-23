1c4c2581d946c3a37caa2a9c5a5cd3f642dacd07
MilanTuryna <miloslavturyna@seznam.cz>
2023-01-23 14:40:15 +0100

    Changed SortManager constructor (new $useAssociativeID, if we want children as classic array or ass. array), setting sortObject level by parent_id logic,

--- php/InfinitySort/SortManager.php
+++ php/InfinitySort/SortManager.php
@@ -15,26 +15,32 @@ class SortManager
     public array $sortedRows;
 
     /**
-     * @param ...$rows
+     * @param array $rows
+     * @param bool $useAssociativeID
      */
-    public function __construct(...$rows) {
+    public function __construct(array $rows, bool $useAssociativeID = false) {
         $this->rows = $rows;
 
-        $this->sort();
+        $this->sort($useAssociativeID);
     }
 
     // TODO: Change variable names, because it's so confusing
+
     /**
+     * @param bool $useAssociativeID
      * @return void
      */
-    private function sort(): void {
-        $levels1 = array_filter($this->rows, fn(sortObject $obj) => $obj->level === 0);
+    private function sort(bool $useAssociativeID = false): void {
+        $levels1 = array_filter($this->rows, fn(sortObject $obj) => $obj->parent_id == 0);
         foreach ($levels1 as $firstLevel) {
+            $firstLevel->level = 0;
             $childrenOf1 = array_filter($this->rows, fn(sortObject $obj) => $obj->parent_id === $firstLevel->id);
             $newEntry = sortObject::init()->createFrom($firstLevel);
             foreach ($childrenOf1 as $key => $child1) {
+                $childrenOf1[$key]->level = 1; $child1->level = 1;
                 $calculateInfinityLevel = function ($level, $otherLevels) use (&$calculateInfinityLevel, &$childrenOf1, $child1, $key) {
                     foreach ($otherLevels as $obj) {
+                        $obj->level = $level;
                         $childrenOf1[$key]->children[] = $obj;
                         $nextLevel = $level + 1;
                         $filter = fn(sortObject $obj) => $obj->level === $nextLevel && $obj->parent_id === $obj->id;
@@ -44,7 +50,7 @@ class SortManager
                 $filter = fn(sortObject $obj) => $obj->level === 2 && $obj->parent_id === $child1->id;
                 $calculateInfinityLevel(2, array_filter($this->rows, $filter));
             }
-            $newEntry->children = $childrenOf1;
+            $newEntry->children = $useAssociativeID ? $childrenOf1 : array_values($childrenOf1);
             $this->sortedRows[] = $newEntry;
         }
     }
